8c968bcce03d0f164bde685abf3b0357cb85924f
massstab <45436293+massstab@users.noreply.github.com>
2023-02-18 11:26:06 +0100

    Update main.py
    Deleted no more used lines

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ import openai
 import datetime
 
 if __name__ == '__main__':
-    # model = whisper.load_model("base")
-    # result = model.transcribe("audio.wav")
-    # print(result["text"])
     python_exec_path = sys.executable
     pro_plot = subprocess.run([python_exec_path, 'audio_realtimeplot.py'])
 
