e618be9f2c8b5f1cd6a92da3b8db3226a42009e7
massstab <dave@xiala.net>
2023-03-08 22:15:06 +0100

    Added Assistant class and made a main function

--- main.py
+++ main.py
@@ -1,51 +1,73 @@
-import subprocess
+from configparser import ConfigParser
 import sys
 import os
+import subprocess
+import logging
 import whisper
 import openai
-import datetime
-from configparser import ConfigParser
 
-def get_API_KEY():
-    config = ConfigParser()
-    config.read('config.cfg')
-    return config.get('auth', 'OPENAI_API_KEY')
 
-if __name__ == '__main__':
+class Assistant:
+    def __init__(self, firstname) -> None:
+        self.firstname = firstname
+        self.model = None
+        self.transscription = None
+
+    def setup_assistant(self):
+        config = ConfigParser()
+        config.read('config.cfg')
+        openai.api_key = config.get('auth', 'OPENAI_API_KEY')
+
+    def load_speech_to_text_model(self, modelname):
+        """For Whisper there are: 'tiny', 'base', 'small' models'"""
+        self.model = whisper.load_model(modelname)
+        
+    def transcribe_audio_file(self, audiofilename):
+        if self.model:
+            self.transscription = self.model.transcribe(audiofilename, fp16=False)
+        else:
+            print('First load a speech_to_text_model')
+
+    def response(self):
+        if self.transscription:
+            response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=self.transscription["text"],
+            temperature=0.7,
+            max_tokens=10,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+            )
+        else:
+            print('First transcribe the audiofile')
+        return response["choices"][0]["text"][2:]
+
+def main():
+    logging.basicConfig(filename='interaction.log', filemode='w', format='%(asctime)s - %(levelname)s - %(message)s',
+                        datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
     python_exec_path = sys.executable
-    pro_plot = subprocess.run([python_exec_path, 'audio_realtimeplot.py'])
+    subprocess.run([python_exec_path, 'audio_realtimeplot.py'])
 
-    # model1 = whisper.load_model("tiny")
-    model2 = whisper.load_model("base")
-#     model3 = whisper.load_model("small")
-#     result1 = model1.transcribe("audio.wav", fp16=False)
-    result2 = model2.transcribe("audio.wav", fp16=False)
-#     result3 = model3.transcribe("audio.wav", fp16=False)
+    print(pro_plot)
 
-    API_KEY = get_API_KEY()
-    openai.api_key = API_KEY
+    howie = Assistant('Howie')
+    howie.setup_assistant()
+    howie.load_speech_to_text_model('base')
+    howie.transcribe_audio_file("audio.wav")
 
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=result2["text"],
-        temperature=0.7,
-        max_tokens=256,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-    )
+    user_text = howie.transscription["text"]
+    print(user_text)
 
-    question = result2["text"]
-    print(f"Question: {question}")
+    logging.info(f'From user to assistant: {user_text}')
 
-    answer = response["choices"][0]["text"][2:]
-    now = datetime.datetime.now()
-    dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
+    assistant_text = howie.response()
+    print(assistant_text)
 
-    with open("interaction.log", "a") as log_file:
-        log_file.write(dt_string + " - " + "Question: " + question + "\n" + dt_string + " - " + "Answer: " + answer + "\n")
-
-    print(f"Answer: {answer}")
+    logging.info(f'From user to assistant: {assistant_text}')
 
     os.remove('audio.wav')
     print("audio.wav has been deleted")
+
+if __name__ == '__main__':
+    main()
