85af71bc41cb01a9c87591200fa80b1bf95dd65d
massstab <dave@xiala.net>
2023-03-09 10:54:15 +0100

    Made class Recorder

--- audio_recording.py
+++ audio_recording.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-"""Create a recording with arbitrary duration.
-
+"""
+Create a recording with arbitrary duration.
 The soundfile module (https://python-soundfile.readthedocs.io/)
 has to be installed!
-
 """
-import tempfile
+
 import queue
 import sys
 
@@ -15,44 +14,45 @@ import numpy  # Make sure NumPy is loaded before it is used in the callback
 
 assert numpy  # avoid "imported but unused" message (W0611)
 
-q = queue.Queue()
 
+class Recorder:
+    def __init__(self) -> None:
+        self.q = queue.Queue()
+        self.channels = [1]  # input channels to plot -> int
+        # input device (numeric ID or substring) -> int_or_str
+        self.device = None
+        self.window = 200  # visible time slot, default=200 ms  -> float
+        self.interval = 30  # minimum time between plot updates  -> float
+        self.blocksize = 1024  # block size (in samples)
+        self.samplerate = None  # sampling rate of audio device  -> float
+        self.downsample = 10  # display every Nth sample, default=10  -> int
+        self.filename = 'audio.wav'  # audio file to store recording to  -> str
+        self.subtype = "PCM_16"  # sound file subtype (e.g. "PCM_24") -> str
+        self.mapping = [c - 1 for c in self.channels]
 
-def callback(indata, frames, time, status):
-    """This is called (from a separate thread) for each audio block."""
-    if status:
-        print(status, file=sys.stderr)
-    q.put(indata.copy())
+    def callback(self, indata, frames, time, status):
+        """This is called (from a separate thread) for each audio block."""
+        if status:
+            print(status, file=sys.stderr)
+        self.q.put(indata.copy())
 
-
-channels = [1]  # input channels to plot -> int
-device = None# input device (numeric ID or substring) -> int_or_str
-window = 200  # visible time slot, default=200 ms  -> float
-interval = 30  # minimum time between plot updates  -> float
-blocksize = 1024  # block size (in samples)
-samplerate = None  # sampling rate of audio device  -> float
-downsample = 10  # display every Nth sample, default=10  -> int
-filename = 'audio.wav'  # audio file to store recording to  -> str
-subtype = "PCM_24"  # sound file subtype (e.g. "PCM_24") -> str
-
-mapping = [c - 1 for c in channels]
-
-try:
-    if samplerate is None:
-        device_info = sd.query_devices(device, 'input')
-        # soundfile expects an int, sounddevice provides a float:
-        samplerate = int(device_info['default_samplerate'])
-    # Make sure the file is opened before recording anything:
-    with sf.SoundFile(filename, mode='x', samplerate=samplerate,
-                      channels=max(channels), subtype=subtype) as file:
-        with sd.InputStream(samplerate=samplerate, device=device,
-                            channels=max(channels), callback=callback):
-            print('#' * 80)
-            print('press esc or close signal window to stop the recording')
-            print('#' * 80)
-            while True:
-                file.write(q.get())
-except KeyboardInterrupt:
-    print('\nRecording finished: ' + repr(filename))
-except Exception as e:
-    print(e)
+    def record(self):
+        try:
+            if self.samplerate is None:
+                device_info = sd.query_devices(self.device, 'input')
+                # soundfile expects an int, sounddevice provides a float:
+                samplerate = int(device_info['default_samplerate'])
+            # Make sure the file is opened before recording anything:
+            with sf.SoundFile(self.filename, mode='x', samplerate=samplerate,
+                              channels=max(self.channels), subtype=self.subtype) as file:
+                with sd.InputStream(samplerate=samplerate, device=self.device,
+                                    channels=max(self.channels), callback=self.callback):
+                    print('#' * 80)
+                    print('press esc or close signal window to stop the recording')
+                    print('#' * 80)
+                    while True:
+                        file.write(self.q.get())
+        except KeyboardInterrupt:
+            print('\nRecording finished: ' + repr(self.filename))
+        except Exception as e:
+            print(e)
