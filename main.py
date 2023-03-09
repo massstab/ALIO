66f0c3ac80c60bd2ae499b36d4c22d99d7966d07
massstab <dave@xiala.net>
2023-03-09 01:09:09 +0100

    Made class TranscriptionHandler

--- main.py
+++ main.py
@@ -8,40 +8,57 @@ import openai
 
 
 class Assistant:
+    """
+    This class represents the assistant with methods that handels the workflow
+    from asking the assistant (a user_text) to the response (assistant_text)
+    """
+
     def __init__(self, firstname) -> None:
         self.firstname = firstname
-        self.model = None
-        self.transscription = None
+        self.usertext = None
 
     def setup_assistant(self):
         config = ConfigParser()
         config.read('config.cfg')
         openai.api_key = config.get('auth', 'OPENAI_API_KEY')
 
-    def load_speech_to_text_model(self, modelname):
-        """For Whisper there are: 'tiny', 'base', 'small' models'"""
-        self.model = whisper.load_model(modelname)
-        
-    def transcribe_audio_file(self, audiofilename):
-        if self.model:
-            self.transscription = self.model.transcribe(audiofilename, fp16=False)
-        else:
-            print('First load a speech_to_text_model')
+    def get_usertext(self, userinput):
+        self.usertext = userinput
 
     def response(self):
-        if self.transscription:
-            response = openai.Completion.create(
+        response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=self.transscription["text"],
+            prompt=self.usertext,
             temperature=0.7,
             max_tokens=10,
             top_p=1,
             frequency_penalty=0,
             presence_penalty=0
-            )
-        else:
-            print('First transcribe the audiofile')
+        )
         return response["choices"][0]["text"][2:]
+
+
+class TranscriptionHandler:
+    def __init__(self) -> None:
+        self.model = None
+        self.transcription = None
+
+    def load_speech_to_text_model(self, modelname):
+        """For Whisper there are: 'tiny', 'base', 'small' models'"""
+        self.model = whisper.load_model(modelname)
+
+    def transcribe_audio_file(self, audiofilename):
+        if self.model:
+            self.transcription = self.model.transcribe(
+                audiofilename, fp16=False)
+        else:
+            print('First load a speech_to_text_model')
+
+
+class RecordingHandler:
+    def __init__(self) -> None:
+        pass
+
 
 def main():
     logging.basicConfig(filename='interaction.log', filemode='w', format='%(asctime)s - %(levelname)s - %(message)s',
@@ -49,14 +66,15 @@ def main():
     python_exec_path = sys.executable
     subprocess.run([python_exec_path, 'audio_realtimeplot.py'])
 
-    print(pro_plot)
-
     howie = Assistant('Howie')
     howie.setup_assistant()
-    howie.load_speech_to_text_model('base')
-    howie.transcribe_audio_file("audio.wav")
 
-    user_text = howie.transscription["text"]
+    transcript = TranscriptionHandler()
+    transcript.load_speech_to_text_model('base')
+    transcript.transcribe_audio_file("audio.wav")
+
+    user_text = transcript.transcription["text"]
+    howie.get_usertext(user_text)
     print(user_text)
 
     logging.info(f'From user to assistant: {user_text}')
@@ -68,6 +86,7 @@ def main():
 
     os.remove('audio.wav')
     print("audio.wav has been deleted")
+
 
 if __name__ == '__main__':
     main()
