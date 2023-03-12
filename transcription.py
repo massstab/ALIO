1c788f1eecad4892f9585ae09cb761211fad71f2
massstab <dave@xiala.net>
2023-03-13 00:25:32 +0100

    dedicated transcription file

---
+++ transcription.py
@@ -0,0 +1,20 @@
+import whisper
+import os
+import logging
+
+
+class TranscriptionHandler:
+    def __init__(self) -> None:
+        pass
+
+    def load_speech_to_text_model(self, modelname):
+        """For Whisper there are: 'tiny', 'base', 'small' models'"""
+        self.model = whisper.load_model(modelname)
+
+    def transcribe_audio_file(self, filename):
+        if self.model:
+            self.transcription = self.model.transcribe(filename, fp16=False)
+            os.remove(filename)
+            logging.info(f"Temporary audiofile {filename} has been deleted")
+        else:
+            print('First load a speech_to_text_model')
