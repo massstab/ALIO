81421ed3c0843e5a3ea96937115dec26e90e5215
massstab <dave@xiala.net>
2023-03-13 23:21:04 +0100

    some error handling with API key

--- assistant.py
+++ assistant.py
@@ -18,13 +18,11 @@ class Assistant:
         try:
             config.read('config.cfg')
             openai.api_key = config.get('auth', 'OPENAI_API_KEY')
-            openai.Completion.create(engine="davinci", prompt="Hello, world!")
         except FileNotFoundError as e:
             logging.error(f"Config file not found: {e}")
-            raise
+
         except KeyError as e:
             logging.error(f"Missing API key in config file: {e}")
-            raise
 
     def get_usertext(self, userinput):
         self.usertext = userinput
@@ -32,15 +30,20 @@ class Assistant:
 
     def response(self, from_user):
         self.get_usertext(from_user)
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=self.usertext,
-            temperature=0.7,
-            max_tokens=5,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0
-        )
-        response_text = response["choices"][0]["text"][2:]  # type: ignore
-        logging.info(f'From assistant to user: {response_text}')
-        return response_text
+        try:
+            response = openai.Completion.create(
+                model="text-davinci-003",
+                prompt=self.usertext,
+                temperature=0.7,
+                max_tokens=5,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0)
+            response_text = response["choices"][0]["text"][2:]  # type: ignore
+            logging.info(f'From assistant to user: {response_text}')
+            return response_text
+
+        except openai.error.AuthenticationError as e:  # type: ignore
+            # Handle authentication error, e.g. check credentials or log
+            print(f"OpenAI API request was not authorized: {e}")
+            return None
